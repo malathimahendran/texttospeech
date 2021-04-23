87974cd1ba5a9e51d68c8db3ad6a478f411b9966
Malathimahendran <malamagendran@gmail.com>
2021-04-23 17:36:02 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [StatusBar,
+    SplashScreen,
+    TextToSpeech,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  text_sentences = [];
+  constructor( private tts: TextToSpeech) {
+    // this.text_sentences = []
+  }
+  textToSpeech(text) {
+    
+    this.tts.speak(text)
+      .then(() => console.log('Success'))
+      .catch((reason: any) => console.log(reason));
+  }
+  fun(text){
+    this.text_sentences = [text]
+  }
 
-  constructor() {}
+
+
 
 }
